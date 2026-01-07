9f431e9355b77c9c105c227e8e4fb95f819bc168
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2026-01-07 13:29:55 +0100

    Remove redundant step to get authorities

--- src/main/java/com/fredande/rewardsappbackend/CustomUserDetailsService.java
+++ src/main/java/com/fredande/rewardsappbackend/CustomUserDetailsService.java
@@ -2,15 +2,11 @@ package com.fredande.rewardsappbackend;
 
 import com.fredande.rewardsappbackend.model.User;
 import com.fredande.rewardsappbackend.repository.UserRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 ///  Define custom behavior for UserDetailsService
 @Service
@@ -23,12 +19,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+    @NonNull
+    public UserDetails loadUserByUsername(@NonNull String userName) throws UsernameNotFoundException {
         User user = userRepository
                 .findByEmailOrUsername(userName, userName)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
         return new CustomUserDetails(user);
     }
 
